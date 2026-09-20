f0b27ace06b70cdc2be5743c1834bd3b72eab358
root <root@localhost.localdomain>
2026-09-21 00:15:34 +0530

    Add Firebase App Check

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ai/firebase_ai.dart';
-import 'firebase_options.dart';
-void main() async {
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'firebase_options.dart';void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const AivoraApp());
+  options:
+ DefaultFirebaseOptions.currentPlatform,
+);
+await FirebaseAppCheck.instance.activate(
+  providerAndroid: const
+ AndroidDebugProvider(),
+);                      
+    runApp(const AivoraApp());
 }
 class AivoraApp extends StatelessWidget {
   const AivoraApp({super.key});
