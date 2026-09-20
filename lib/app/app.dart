8b5671c58690adadb3d26305728ea1c8459b3f8f
mangyar1122-ship-it <mangyar1122@gmail.com>
2026-09-20 14:57:02 +0530

    Connect AIVORA app to professional home screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../core/constants/app_constants.dart';
 import '../core/theme/app_theme.dart';
+import '../features/home/home_page.dart';
 
 class AivoraApp extends StatelessWidget {
   const AivoraApp({super.key});
@@ -10,43 +11,11 @@ class AivoraApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
       title: AppConstants.appName,
-
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
-
-      home: const AivoraHome(),
-    );
-  }
-}
-
-class AivoraHome extends StatelessWidget {
-  const AivoraHome({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(AppConstants.appName),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              AppConstants.appName,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const SizedBox(height: 8),
-            Text(
-              AppConstants.tagline,
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-          ],
-        ),
-      ),
+      home: const HomePage(),
     );
   }
 }
