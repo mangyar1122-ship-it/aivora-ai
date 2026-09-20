53909d6c8520988386b787a5bbf86e1f5720e271
mangyar1122-ship-it <mangyar1122@gmail.com>
2026-09-20 09:27:11 +0530

    Update app_constants.dart

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,1 +1,17 @@
+class AppConstants {
+  AppConstants._();
 
+  // App Identity
+  static const String appName = 'AIVORA AI';
+  static const String tagline = 'Create. Chat. Imagine.';
+
+  // Package
+  static const String packageName = 'com.aivora.ai';
+
+  // App Version
+  static const String version = '1.0.0';
+
+  // General
+  static const String defaultLanguage = 'en';
+  static const String supportEmail = 'support@aivora.ai';
+}
